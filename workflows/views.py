6768e8643f392ffadc10b0a32d264f38b3c0c053
paulmunyao <paulmunyao094@gmail.com>
2022-06-20 10:14:56 +0300

    Using function based views to return the response

--- workflows/views.py
+++ workflows/views.py
@@ -2,5 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+# Using function based views to return the response
 def index(request):
     return HttpResponse("Hello, world. You're at the polls index.")
