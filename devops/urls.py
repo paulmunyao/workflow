fd4614de1eec6887bb656347b7b80f4262bd366a
paulmunyao <paulmunyao094@gmail.com>
2022-06-20 10:13:53 +0300

    Set url path for the application inside the project

--- devops/urls.py
+++ devops/urls.py
@@ -18,5 +18,7 @@ from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
+    # Set url path for the application inside the project
+    path('workflows/', include(workflows.urls)),
     path('admin/', admin.site.urls),
 ]
