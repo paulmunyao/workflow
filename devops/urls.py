1644a2eb6f29f7815b7bb1ba8532639e5b9e0263
paulmunyao <paulmunyao094@gmail.com>
2022-06-20 10:11:08 +0300

    Document code

--- devops/urls.py
+++ devops/urls.py
@@ -14,6 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+# import include to reference the app's location
 from django.urls import include, path
 
 urlpatterns = [
