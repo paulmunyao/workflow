921eedf18a7652796fe6e6e5cbc8ecc84f49ae05
paulmunyao <paulmunyao094@gmail.com>
2022-06-22 22:11:08 +0300

    Modify files on branch

--- workflows/views.py
+++ workflows/views.py
@@ -4,4 +4,4 @@ from django.http import HttpResponse
 # Create your views here.
 # Using function based views to return the response
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+    return HttpResponse("Hello, world. You're at the polls index.I'm a devops engineer")
